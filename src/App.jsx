bd40c7eeca26f82982aa9cb89582a1659ef1e9d3
GonzaSotelo <g.sotelo.dab@gmail.com>
2024-02-10 23:42:57 -0300

    prueva

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>
+      <h1 className='h1-home'>
         hola mundo
       </h1>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,26 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+
+
+const bgAnimation = document.getElementById('bgAnimation');
+const numberOfColumns = 23; // Número de columnas
+const numberOfRows = 23; // Número de filas
+const hexagonSize = 70; // Tamaño de los hexágonos
+
+for (let row = 0; row < numberOfRows; row++) {
+    for (let col = 0; col < numberOfColumns; col++) {
+        const colorBox = document.createElement('div');
+        colorBox.classList.add('colorBox');
+
+        // Posicionamiento de los hexágonos en filas y columnas
+        const x = col * (hexagonSize * 1.5); // Ajusta el 1.5 para la distancia entre hexágonos
+        const y = row * (hexagonSize * Math.sqrt(2.3)); // Ajusta el Math.sqrt(3) para la distancia vertical
+
+        colorBox.style.left = `${x}px`;
+        colorBox.style.top = `${y}px`;
+
+        bgAnimation.append(colorBox);
+    }
+}
